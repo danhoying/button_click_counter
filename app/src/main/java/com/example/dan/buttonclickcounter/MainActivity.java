60129e00c0a1060355eac5b8e2f682989074917e
Dan Hoying <danhoying@gmail.com>
2017-02-03 12:07:15 -0800

    Add button click counter functionality

--- app/src/main/java/com/example/dan/buttonclickcounter/MainActivity.java
+++ app/src/main/java/com/example/dan/buttonclickcounter/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.dan.buttonclickcounter;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText userInput;
     private Button button;
     private TextView textView;
+    private int numTimesClicked = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +22,14 @@ public class MainActivity extends AppCompatActivity {
         userInput = (EditText) findViewById(R.id.editText);
         button = (Button) findViewById(R.id.button);
         textView = (TextView) findViewById(R.id.textView);
+        View.OnClickListener ourOnClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numTimesClicked = numTimesClicked + 1;
+                String result = "\nThe button got tapped " + numTimesClicked + " time";
+                textView.append(result);
+            }
+        };
+        button.setOnClickListener(ourOnClickListener);
     }
 }
